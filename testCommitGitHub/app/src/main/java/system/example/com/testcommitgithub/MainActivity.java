854a112ccbc7db883af9b5e051bb11728a0ff1f6
gonglipeng <1101144920@qq.com>
2017-03-23 13:22:41 +0800

    提交测试github

---
+++ testCommitGitHub/app/src/main/java/system/example/com/testcommitgithub/MainActivity.java
@@ -0,0 +1,13 @@
+package system.example.com.testcommitgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
